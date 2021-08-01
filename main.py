01976a8a100391f28dffbe6346f0bdb848b71dce
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2021-08-01 22:11:49 +0200

    Working example with 4 operators.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
+from copy import copy
 from dataclasses import dataclass
 from operator import add, floordiv, mul, sub
-from tkinter import Button, Entry, Tk
+from tkinter import Button, Entry, StringVar, Tk
 from typing import Any, Callable, Union
 
 class App:
 	def __init__(self):
 		self.root = Tk()
 		self.root.title("Calculator")
-		self.entry = Entry(self.root, width=44)
-		self.entry.grid(row=0, column=0, columnspan=5)
-		self.lastItem: CalcItem = None
+		self.entryVar = StringVar()
+		Entry(self.root, width=44, state="readonly", textvariable=self.entryVar
+			).grid(row=0, column=0, columnspan=5)
+		self.reset()
 
 		for i in range(9):
 			CalcValBtn(self, 3 - i // 3, i % 3, CalcVal(i + 1))
@@ -24,18 +26,39 @@ class App:
 	def addItem(self, val: CalcVal):
 		if self.lastItem is None:
 			self.lastItem = CalcItem(None, val)
+			self.firstItem = self.lastItem
 		elif not self.lastItem.appendDigit(val):
-			self.lastItem = CalcItem(self.lastItem, val)
+			next = CalcItem(self.lastItem, val)
+			self.lastItem.next = next
+			self.lastItem = next
+		self.updateEntry()
 
-	def print(self):
-		currItem = self.lastItem
+	def genItems(self):
+		currItem = self.firstItem
 
 		while currItem is not None:
-			print(currItem.val)
-			currItem = currItem.prev
+			yield currItem
+			currItem = currItem.next
+
+	def genItemStrs(self):
+		for item in self.genItems():
+			yield str(item.val)
+
+	def itemsByHighPriority(self):
+		return sorted(self.genItems(), key=lambda x: -x.priority)
+
+	def reset(self):
+		self.firstItem: CalcItem = None
+		self.lastItem: CalcItem = None
 
 	def run(self):
 		self.root.mainloop()
+
+	def showRes(self, res: Union[int, RuntimeError, str]):
+		self.entryVar.set(str(res))
+
+	def updateEntry(self):
+		self.entryVar.set(" ".join(self.genItemStrs()))
 
 class Btn:
 	def __init__(self, app: App, row: int, col: int, span: int, name: str):
@@ -53,28 +76,73 @@ class CalcItem:
 	val: CalcVal
 
 	def __post_init__(self):
+		self.next: CalcItem = None
 		self.priority = self.val.priority()
 
-	def appendDigit(self, val: CalcVal) -> bool:
+	def appendDigit(self, val: CalcVal):
 		return self.val.appendDigit(val)
+
+	def delete(self):
+		self.prev = None
+		self.next = None
+
+	def deleteOp(self):
+		self.prev.delete()
+		self.next.delete()
+		self.delete()
+
+	def eval(self):
+		self.reqOp()
+		newNext = self.next.next
+		newPrev = self.prev.prev
+		new = CalcItem(newPrev, CalcVal(
+			self.val(self.prev.toInt(), self.next.toInt())))
+		new.next = newNext
+
+		setPrev(newNext, new)
+		setNext(newPrev, new)
+		self.deleteOp()
+		return new
+
+	def isInt(self):
+		return self.val.isInt()
+
+	def isOp(self):
+		return not self.isInt()
+
+	def reqOp(self):
+		if self.isInt():
+			raise RuntimeError(f"{self.val} is not an operator.")
+		if self.next is None or self.prev is None:
+			raise RuntimeError(
+				f"Operator {self.val} doesn't have both neighbors.")
+
+	def toInt(self):
+		if self.isOp():
+			raise RuntimeError(
+				f"Operator {self.val} is not a number.")
+		return self.val.val
 
 @dataclass
 class CalcVal:
 	val: Union[int, Operator]
 
+	def __call__(self, a, b) -> int:
+		return self.val(a, b)
+
 	def __str__(self):
 		return str(self.val) if self.isInt() else opInfo[self.val].name
 
-	def appendDigit(self, o: CalcVal) -> bool:
+	def appendDigit(self, o: CalcVal):
 		if self.isInt() and o.isInt():
 			self.val = self.val * 10 + o.val
 			return True
 		return False
 
-	def isInt(self) -> bool:
+	def isInt(self):
 		return isinstance(self.val, int)
 
-	def priority(self) -> int:
+	def priority(self):
 		return 0 if self.isInt() else opInfo[self.val].priority
 
 class CalcValBtn(Btn):
@@ -85,14 +153,33 @@ class CalcValBtn(Btn):
 		self.val = val
 
 	def onClick(self):
-		self.app.addItem(self.val)
+		self.app.addItem(copy(self.val))
+
+def evalItems(items: list[CalcItem]):
+	try:
+		if not items:
+			return "No input."
+		if len(items) == 1:
+			return f"Sole number: {items[0].toInt()}"
+
+		res = items[0]
+
+		for item in items:
+			if item.isInt():
+				return res.toInt()
+			res = item.eval()
+	except RuntimeError as e:
+		return e
+	except ZeroDivisionError:
+		return "Division by zero."
 
 class EvalBtn(Btn):
 	def __init__(self, app: App, row: int, col: int, span: int):
 		super().__init__(app, row, col, span, "=")
 
 	def onClick(self):
-		self.app.print()
+		self.app.showRes(evalItems(self.app.itemsByHighPriority()))
+		self.app.reset()
 
 Operator = Callable[[Any, Any], Any]
 
@@ -108,5 +195,13 @@ opInfo = {
 	floordiv: OpInfo("/", 2)
 }
 
-if __name__ == '__main__':
+def setNext(item: CalcItem, next: CalcItem):
+	if item is not None:
+		item.next = next
+
+def setPrev(item: CalcItem, prev: CalcItem):
+	if item is not None:
+		item.prev = prev
+
+if __name__ == "__main__":
 	App().run()
